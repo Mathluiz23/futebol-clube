99c7bb04ec2ee10fb62bc6f5aeb7bda2f38e9b81
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-20 11:11:12 -0300

    creating and configuring model Club

---
+++ app/backend/src/database/models/Club.ts
@@ -0,0 +1,28 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Club extends Model {
+  static associate(models: any) {
+    Club.hasMany(models.Match, { foreignKey: 'id', as: 'matchs' });
+  }
+}
+
+Club.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  club_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Club',
+  tableName: 'clubs',
+  timestamps: false,
+});
+
+export default Club;
