4c2e0dcc78c2ec5075af8bad3a23f53fd0ec40af
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-20 12:57:56 -0300

    implementing token file, creating and validating token

---
+++ app/backend/src/database/utils/token.ts
@@ -0,0 +1,17 @@
+import * as jwt from 'jsonwebtoken';
+
+const jwtSecret = 'super_senha';
+
+export async function createToken(payload:any) {
+  const token = jwt.sign(payload, jwtSecret, { algorithm: 'HS256', expiresIn: '15d' });
+  return token;
+};
+
+export function validateToken(token:string): any {
+  try {
+    jwt.verify(token, jwtSecret);
+    return jwt.decode(token, { complete: true });
+  } catch (error) {
+      return false;
+  }
+};
