0f5a4ad22da5c330d515aafb7ea080a76d836a2b
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-23 09:16:06 -0300

    creating function validatePassword in file token

--- app/backend/src/utils/token.ts
+++ app/backend/src/utils/token.ts
@@ -1,17 +1,24 @@
 import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcryptjs';
+import User from '../database/models/User';
 
 const jwtSecret = 'super_senha';
+
+export async function validatePassword(string: string | null, hash:string | null) {
+  return !bcrypt.compare(string
+ || 'ffh', hash || 'asd');
+}
 
 export async function createToken(payload:any) {
   const token = jwt.sign(payload, jwtSecret, { algorithm: 'HS256', expiresIn: '15d' });
   return token;
-};
+}
 
 export function validateToken(token:string): any {
   try {
     jwt.verify(token, jwtSecret);
     return jwt.decode(token, { complete: true });
   } catch (error) {
-      return false;
+    return false;
   }
-};
+}
