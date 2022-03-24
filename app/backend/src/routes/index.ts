939ef4f3502a044fa1d4857ccf647d43258c05e8
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-24 10:14:48 -0300

    adding route clubs in index file of routes folder

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 
 import loginRoute from './loginRoute';
+import clubRoute from './clubRoute';
 
 const route = express.Router();
 
 route.use('/login', loginRoute);
+
+route.use('/clubs', clubRoute);
 
 export default route;
