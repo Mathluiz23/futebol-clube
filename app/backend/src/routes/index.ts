3522752155f3212237fdea6f9dc93baf8bba02ca
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-27 16:16:45 -0300

    creating matchs route in index file

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -2,11 +2,14 @@ import * as express from 'express';
 
 import loginRoute from './loginRoute';
 import clubRoute from './clubRoute';
+import matchRoute from './matchRoute';
 
 const route = express.Router();
 
 route.use('/login', loginRoute);
 
 route.use('/clubs', clubRoute);
+
+route.use('/matchs', matchRoute);
 
 export default route;
