89fef5f78acddce80ac468bc5b3ec5d1e881a90a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-28 17:15:12 -0300

    implements createMatchs in matchsController

--- app/backend/src/controller/matchController.ts
+++ app/backend/src/controller/matchController.ts
@@ -12,3 +12,9 @@ export async function getById(req: Request, res: Response): Promise<void> {
   const userFinded: ResponseStatus = await matchService.getbyId(+req.params.id);
   res.status(userFinded.status).json(userFinded.response);
 }
+
+export async function createMatch(req: Request, res: Response): Promise<void> {
+  const MatchCreated = await matchService.createMatch(req);
+
+  res.status(MatchCreated.status).json(MatchCreated.response);
+}
