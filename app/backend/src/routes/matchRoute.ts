e055028960e5af4c788ea735c8b9147a9bf589f5
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-03-28 18:49:42 -0300

    implements update function in the matchs-id route of the patch verb

--- app/backend/src/routes/matchRoute.ts
+++ app/backend/src/routes/matchRoute.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
-import { getAll, getById, createMatch, matchFineshed } from '../controller/matchController';
+import { getAll, getById, createMatch, matchFineshed, update } from '../controller/matchController';
 
 const route = express.Router();
 
 route.patch('/:id/finish', matchFineshed);
+route.patch('/:id', update);
 route.get('/:id', getById);
 route.get('/', getAll);
 route.post('/', createMatch);
